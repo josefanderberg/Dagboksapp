03bdcaafc84be8575756654050db850f79aae0dd
Josef Anderberg <josef.anderberg@gmail.com>
2025-09-29 16:57:32 +0200

    Add switch menu

--- Program.cs
+++ Program.cs
@@ -5,8 +5,42 @@ class Program
 {
     public void Main(string[] args)
     {
+        Console.WriteLine("=== DAGBOKSAPP ===");
+        Console.WriteLine("1. Skriv ny anteckning");
+        Console.WriteLine("2. Lista alla anteckningar");
+        Console.WriteLine("3. Sök anteckning på datum");
+        Console.WriteLine("4. Spara till fil");
+        Console.WriteLine("5. Läs från fil");
+        Console.WriteLine("6. Avsluta");
 
+        while (true)
+        {
+            if (int.TryParse(Console.ReadLine(), out int choice))
 
+                switch (choice)
+                {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+
+                    case 6:
+
+                        break;
+                    default:
+                        break;
+                }
+        }
     }
 }
-
